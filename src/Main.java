42753df9fbd9388eb1636fd6fe19227bbfa3156e
“Ignatiy <INayanov@inno.tech>
2025-06-19 18:17:10 +0400

    Simple calculator was added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма: " + (firstNumber + secondNumber));
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + (firstNumber * secondNumber));
+        System.out.println("Частное: " + ((double)firstNumber / secondNumber));
     }
+
 }
