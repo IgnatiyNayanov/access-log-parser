c99be221709a5e3ef06b402f23d8cfd9a7c28ee8
“Ignatiy <INayanov@inno.tech>
2025-07-01 20:58:46 +0400

    Checking of path to the file was added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+//        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                System.out.println("Файл не существует или указан путь к папке");
+                count++;
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+            }
+            count++;
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
