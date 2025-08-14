923f6131dfd2f119718ad689ab1c37cccdd1cea3
“Ignatiy <INayanov@inno.tech>
2025-08-14 17:50:38 +0400

    Reading of file was added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Случайное число от 0 до 1: " + Math.random());
         int count = 0;
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            boolean fileExists = file.exists();
+            boolean isFileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (!fileExists || isDirectory) {
+            if (!isFileExists || isDirectory) {
                 System.out.println("Файл не существует или указан путь к папке");
                 count++;
                 continue;
@@ -21,6 +23,39 @@ public class Main {
             }
             count++;
             System.out.println("Это файл номер " + count);
+/*Задание #1. Обработка исключений */
+            int totalLines = 0;
+            int maxLength = 0;
+            int minLength = 1024;
+            try (FileReader fileReader = new FileReader(path);
+                 BufferedReader reader = new BufferedReader(fileReader)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    totalLines++;
+                    int length = line.length();
+                    if (length > 1024) {
+                        throw new LineTooLongException("Ошибка. Строка № " + totalLines + " превышает максимальную допустимую длину 1024 символов");
+                    }
+
+                    if (length > maxLength) {
+                        maxLength = length;
+                    }
+
+                    if (length < minLength) {
+                        minLength = length;
+                    }
+                }
+
+                System.out.println("Общее количество строк в файле: " + totalLines);
+                System.out.println("Длина самой длинной строки: " + maxLength);
+                System.out.println("Длина самой короткой строки: " + minLength);
+
+            } catch (RuntimeException e) {
+                System.err.println(e.getMessage());
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+            /*Задание #1. Обработка исключений*/
         }
     }
 }
